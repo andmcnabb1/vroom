4640d1eb61f252ad812b5f73eddde442a94b83bb
andmcnabb1 <573733@student.saxion.nl>
2025-05-16 16:04:51 +0200

    working on line locating system

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,30 @@
 #include <Arduino.h>
+int in_dist = 11;
+int out_dist = in_dist + 25;
+int irled = 2;
+int bright1base = 0;
+int bright2base = 0;
+int bright3base = 0;
+int bright4base = 0;
+int bright1 = 0;
+int bright2 = 0;
+int bright3 = 0;
+int bright4 = 0;
+int location = 0;
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(2, OUTPUT);
+}
+void loop()
+{
 
-serialEventRu;
-a hallo dit is nieuw
+    bright1 = 1023 - analogRead(A1);
+    bright2 = 1023 - analogRead(A2);
+    bright3 = 1023 - analogRead(A3);
+    bright4 = 1023 - analogRead(A4);
+    
+    location = (bright1 * out_dist + bright2 * in_dist - bright2 * in_dist - bright3 * out_dist) / (bright1 + bright2 + bright3 + bright4);
+    Serial.print(">bright:");
+    Serial.println(location);
+}
