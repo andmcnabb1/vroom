e98b4e6319ddee795e7f65f16c71635cb89e6a6f
andmcnabb1 <573733@student.saxion.nl>
2025-05-30 15:07:56 +0200

    soo uhhh did some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,55 +4,72 @@ Servo myservo;
 const int in_dist = 11;
 const int out_dist = in_dist + 25;
 const int irled = 2;
-const int threshold = 200;
+const int threshold = 400;
 const int hallpin = 3;
 const int enamotor = 4;
 const int throttle = 5;
 const int tiltswitch = 6;
 const int lowspeed = 5;
 const int highspeed = 10;
-long speed = 0;
+const int steeringoffset = 0;
 
 bool finished = false;
 int locator();
-int pid(int setpoint, int measure,int P,int I,int D);
+int pid(int setpoint, int measure, int P, int I, int D);
 void counter();
 void powercontrol();
 void setup()
 {
-    attachInterrupt(digitalPinToInterrupt(hallpin), counter, RISING);
     pinMode(2, OUTPUT);
     myservo.attach(2);
+    Serial.begin(9600);
 }
 void loop()
 {
     static int lineloc = 0;
     lineloc = locator();
-    myservo.write(pid(0, lineloc,1,0,0));
+    myservo.write(pid(0, lineloc, 10, 0, 0) + 90 + steeringoffset);
+    delay(10);
     powercontrol();
+    counter();
 }
 int locator()
 {
-    int prevloc = 0;
-    int bright1 = 1023 - analogRead(A1);
-    int bright2 = 1023 - analogRead(A2);
-    int bright3 = 1023 - analogRead(A3);
-    int bright4 = 1023 - analogRead(A4);
-    if ((bright1 + bright2 + bright3 + bright4) > threshold)
+    int location = 0;
+
+    bool bright1 = analogRead(A1) < threshold;
+    bool bright2 = analogRead(A2) < threshold;
+    bool bright3 = analogRead(A3) < threshold;
+    bool bright4 = analogRead(A4) < threshold;
+
+    if (bright1)
     {
-        finished = true;
-        return 0;
+        location = -2;
+    }
+    else if (bright2)
+    {
+        location = -1;
+    }
+    else if (bright3)
+    {
+        location = 1;
+    }
+    else if (bright4)
+    {
+        location = 2;
     }
     else
     {
-        int location = (bright1 * out_dist + bright2 * in_dist - bright3 * in_dist - bright4 * out_dist) / (bright1 + bright2 + bright3 + bright4);
-        if (prevloc > 10 and location == 0)
-
-            prevloc = location;
-        return location;
+        location = 0;
     }
+    if (bright1 && bright2 && bright3 && bright4)
+    {
+        finished == true;
+    }
+    return location;
 }
-int pid(int setpoint, int measure,int P,int I,int D)
+
+int pid(int setpoint, int measure, int P, int I, int D)
 {
     int preverror = 0;
     int prevtime = 0;
@@ -71,11 +88,20 @@ int pid(int setpoint, int measure,int P,int I,int D)
 void counter()
 {
     static long prevt = 0;
-    long time = micros();
-    long deltatime = (time - prevt) * 10 ^ -6;
-    speed = (1 / deltatime) * 27.018;
-    prevt = time;
+    static bool haller = true;
+    static float speed = 0;
+    bool prevhal = haller;
+    haller = digitalRead(hallpin);
+    if (haller == false && haller != prevhal)
+    {
+        long time = millis();
+        float deltatime = (time - prevt) *0.001;
+        speed = (1 / deltatime) * 0.27018;
+        prevt = time;
+    }
+    Serial.println(speed);
 }
+
 void powercontrol()
 {
     int setspeed = 0;
@@ -93,8 +119,8 @@ void powercontrol()
     }
     else
     {
-       digitalWrite(enamotor,HIGH);
+        digitalWrite(enamotor, HIGH);
     }
-    
-    analogWrite( throttle , pid(setspeed, speed,1,0,0));
+
+    analogWrite(throttle, pid(setspeed, 0, 1, 0, 0));
 }
