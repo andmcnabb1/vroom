285519b001ef685adeb68630babff9dfe36da950
andmcnabb1 <573733@student.saxion.nl>
2025-06-19 11:46:08 +0200

    hiiiiiiiii

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,10 @@ const int out_dist = in_dist + 25;
 const int irled = 2;
 const int threshold = 600;
 const int stopthreshold = 670;
-const int hallpin = 3;
 const int enamotor = 4;
 const int throttle = 5;
 const int tiltswitch = 6;
-const int lowspeed = 5;
-const int highspeed = 10;
+const int speed = 20;
 const int steeringoffset = 0;
 const int trigPin = 6;
 const int echoPin = 7;
@@ -25,8 +23,6 @@ bool finished = false;
 bool butti = false;
 int locator();
 int pid(int setpoint, int measure, int P, int I, int D);
-// void counter();
-int distancer(int pingtime);
 void powercontrol(int distance);
 void echoCheck();
 void setup()
@@ -46,12 +42,11 @@ void loop()
 
     lineloc = locator();
     myservo.write(pid(0, lineloc, 7, 0, 0) + 90 + steeringoffset);
-    /*if (millis() >= pingTimer)
+    if (millis() >= pingTimer)
     {
         pingTimer += pingSpeed;
         sonar.ping_timer(echoCheck);
-    }*/
-    //distance=100; //this deactivates the ultrasonic sensor
+    }
     Serial.println(distance);
     powercontrol(distance);
     butti = digitalRead(buttonpin);
@@ -92,7 +87,6 @@ int locator()
     if ((bright1 < stopthreshold) && (bright2 < stopthreshold) && (bright3 < stopthreshold) && (bright4 < stopthreshold))
     {
         finished = true;
-        //delay(200); // use if it doesn't stop correctly
     }
     return location;
 }
@@ -118,38 +112,23 @@ int pid(int setpoint, int measure, int P, int I, int D)
 
     return result;
 }
-/* void counter()
-{
-    static long prevt = 0;
-    static bool haller = true;
-    static float speed = 0;
-    bool prevhal = haller;
-    haller = digitalRead(hallpin);
-    if (haller == false && haller != prevhal)
-    {
-        long time = millis();
-        float deltatime = (time - prevt) * 0.001;
-        speed = (1 / deltatime) * 0.27018;
-        prevt = time;
-    }
-} */
 
 void powercontrol(int distance)
 {
 
     if ((!finished) && butti)
     {
-        analogWrite(throttle, 47);
+        analogWrite(throttle, speed);
     }
     else
     {
         analogWrite(throttle, 0);
     }
 }
-/*void echoCheck()
+void echoCheck()
 {
     if (sonar.check_timer())
     {
         distance = sonar.ping_result / US_ROUNDTRIP_CM;
     }
-} */
+}
